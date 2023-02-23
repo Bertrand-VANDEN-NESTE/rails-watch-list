d122d663d671d640eee66c37fc141cd901f4ddc7
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2023-02-23 15:49:58 +0100

    second

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates :movie, presence: true, uniqueness: true
-  validates :list, presence: true, uniqueness: true
-  validates :comment, presence: true, length: { minimum: 6 }
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
   validates :overview, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
